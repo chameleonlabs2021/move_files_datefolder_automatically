2334f2001473ab3ee18c9d74e4f40e69d286c4ea
chameleonlabs2021 <96870965+chameleonlabs2021@users.noreply.github.com>
2022-02-06 21:06:12 +0800

    move_files_datefolder_automatically

---
+++ sorted_files_year_fully_auto.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+fil1=listfile.txt
+move=move.txt
+
+check_year()
+{
+if [[ $1 =~ ^[0-9]{4}$ ]];   then
+echo $1
+return $1
+else
+return 1
+fi
+}
+
+#list all the files 
+find . -type f -ls > $fil1
+
+
+while read -r line;
+do
+echo "line: $line"
+sleep 1
+   year=`echo "$line" |awk -F ' ' '{print $10}'`
+   file=`echo "$line" |awk '{out=""; for(i=11;i<=NF;i++){out=out" "$i}; print out}'` 
+   echo "year : $year"
+   chk=`check_year "$year"`
+echo "year: $year" 
+echo "check: $chk" 
+sleep 1
+echo "file: $file"
+    if [[ ${chk} == $year ]]
+    then 
+    echo "creating the folder ${year}"
+    mkdir ${year}
+    echo "moving files to the folder ${year}"
+    # echo "mv $file ../sorted_photos/$year/ " >> $move
+    mv -f $file ${year}/
+    else
+    echo "moving files to the folder 2022"
+    # echo "mv $file ../sorted_photos/2022/ " >> 2022.txt
+    mv -f $file 2022/
+    fi
+done < ${fil1}
